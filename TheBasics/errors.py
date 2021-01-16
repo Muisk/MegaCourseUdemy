afc10282b48c2d82a0a969e65b6ece954c4865eb
lodurr <ing.diegocordero@gmail.com>
2021-01-16 12:15:54 -0400

    WebPage

---
+++ TheBasics/errors.py
@@ -0,0 +1,8 @@
+a = 1
+b = "2"
+c = 15
+try:
+    print(c/0)
+except ZeroDivisionError:
+    print ("Can't divide by 0") 
+print(int(2.5))    
---
+++ TheBasics/handleError.py
@@ -0,0 +1,9 @@
+def divide (a,b):
+    try:
+        return a/b
+    except ZeroDivisionError:
+        print("Zero division try another one")
+    
+
+print(divide(1,0))
+print("End of program")
